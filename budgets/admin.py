835cded97a51058e673ed6e65bed9627e698de0b
DiogoBlanco <diogoblanco@outlook.com.br>
2023-08-18 10:43:14 -0300

    feat: init

--- budgets/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Budget
-
-# Register your models here.
-
-
-class BudgetAdmin(admin.ModelAdmin):
-    admin.site.register(Budget)
--- home/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Customer
-
-
-class CustomerAdmin(admin.ModelAdmin):
-    admin.site.register(Customer)

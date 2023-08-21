96155d89a0b8e5b887551fd7003d14d7fe8ecb99
DiogoBlanco <diogoblanco@outlook.com.br>
2023-08-21 12:49:38 -0300

    feat: initial

--- next_elevadores/urls.py
+++ next_elevadores/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('clientes/', include('clientes.urls')),
+    path('contratos/', include('contratos.urls')),
+    path('financeiro/', include('financeiro.urls')),
+    path('modernizacao/', include('modernizacao.urls')),
+    path('orcamentos/', include('orcamentos.urls')),
+    path('produtos/', include('produtos.urls')),
 ]

87a6e570c44b299b35cf1ed52b457c0f14f53152
DiogoBlanco <diogoblanco@outlook.com.br>
2023-10-02 09:56:39 -0300

    02/10/23

--- contratos/models.py
+++ contratos/models.py
@@ -18,8 +18,28 @@ class ContratoAnual(models.Model):
                  ('36 meses', '36 meses'), ('48 meses', '48 meses')],
         max_length=8, null=True)
     data_de_renovacao = models.DateField(verbose_name='Data de Renovação')
-    mes_reajuste = models.DateField(verbose_name='Mês de Reajuste')
+    mes_reajuste = models.CharField(
+        choices=[
+            ('Janeiro', 'Janeiro'), ('Fevereiro', 'Fevereiro'), ('Março', 'Março'),
+            ('Abril', 'Abril'), ('Maio', 'Maio'), ('Junho', 'Junho'),
+            ('Julho', 'Julho'), ('Agosto', 'Agosto'), ('Setembro', 'Setembro'),
+            ('Outubro', 'Outubro'), ('Novembro',
+                                     'Novembro'), ('Dezembro', 'Dezembro')
+        ],
+        max_length=9
+    )
     tipo_contrato = models.ForeignKey(TipoContrato, on_delete=models.CASCADE)
+    marca_dos_elevadores = models.CharField(
+        choices=[
+            ('Atlas Schindler', 'Atlas Schindler'), ('Otis', 'Otis'),
+            ('Hyundai', 'Hyundai'), ('Thyessen', 'Thyessen'),
+            ('Villarta', 'Villarta')
+        ],
+        max_length=15
+    )
+    quantidade_de_elevadores = models.PositiveIntegerField()
+    modelo_dos_elevadores = models.CharField(max_length=99)
+    observacoes = models.TextField(max_length=999)
 
     def __str__(self):
         return f'Contrato de {self.cliente.nome} {self.cliente.sobrenome}'

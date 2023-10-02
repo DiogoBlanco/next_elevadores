272a2497b1638d912cf1cca714685e537f14f542
DiogoBlanco <diogoblanco@outlook.com.br>
2023-10-02 18:13:26 -0300

    contratos

--- contratos/urls.py
+++ contratos/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
     path('criar_contrato/', views.criar_contrato, name='criar_contrato'),
     path('detalhes_contrato/<int:contrato_id>/', views.detalhes_contrato,
          name='detalhes_contrato'),
+    path('atualizar/<int:contrato_id>/',
+         views.atualizar_contrato,
+         name='atualizar_contrato'),
+    path('apagar/<int:contrato_id>/',
+         views.apagar_contrato,
+         name='apagar_contrato'),
 ]
--- contratos/views.py
+++ contratos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ContratoForm
 from .models import ContratoAnual
 
@@ -29,3 +29,28 @@ def detalhes_contrato(request, contrato_id):
         'contrato': contrato
     }
     return render(request, 'contratos/pages/detalhes_contratos.html', context)
+
+
+def atualizar_contrato(request, contrato_id):
+    contrato = get_object_or_404(ContratoAnual, pk=contrato_id)
+    form = ContratoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/contratos/detalhes_contrato' + '/' +
+                        str(contrato_id) + '/')
+    context = {
+        'contrato': contrato,
+        'form': form
+    }
+    return render(request, 'contratos/pages/atualizar_contrato.html', context)
+
+
+def apagar_contrato(request, contrato_id):
+    contrato = get_object_or_404(ContratoAnual, pk=contrato_id)
+    if request.method == 'POST':
+        contrato.delete()
+        return redirect('/contratos')
+    context = {
+        'contrato': contrato
+    }
+    return render(request, 'contratos/pages/apagar_contrato.html', context)
